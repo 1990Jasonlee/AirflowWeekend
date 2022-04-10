a35a3e183a20e283464030522d129f9aa749c1e9
Jason <leejason1990@gmail.com>
2022-04-09 23:39:46 -0400

    stuck, broken dag

--- airflow-proj-files/dags/script.py
+++ airflow-proj-files/dags/script.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from matplotlib import pyplot as plt
-
+#import matplotlib
+#matplotlib.use('Agg')
+#from matplotlib import pyplot as plt
+import plotly.express as px
 
 def some_script():
     data = pd.read_csv('/Users/jason/dev/AirflowWeekend/data/pokedex.csv')
@@ -8,17 +10,28 @@ def some_script():
     count = df.groupby('type_1')['type_1'].count()
 
     count.plot.bar()
-    plt.title('Number of Pokemon by Primary Types')
-    plt.xlabel('Primary Type')
-    plt.ylabel('Number of Pokemon')
+    #plt.title('Number of Pokemon by Primary Types')
+    #plt.xlabel('Primary Type')
+    #plt.ylabel('Number of Pokemon')
+    #plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/1')
 
-    plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/1')
+    fig = px.bar(count, y='type_1')
+    fig.update_layout(xaxis_title='Types',
+                      yaxis_title='Number of Pokemon',
+                      title='Pokemon by Types')
+    fig.write_image(f'/Users/jason/dev/AirflowWeekend/data/1')
 
     df1 = data[['name', 'generation']]
     bins = [1, 2, 3, 4, 5, 6, 7, 8]
     df1['generation'].hist(bins=bins)
-    plt.title('Number of Pokemon by Generation')
-    plt.xlabel('Generation')
-    plt.ylabel('Number of Pokemon')
+    #plt.title('Number of Pokemon by Generation')
+    #plt.xlabel('Generation')
+    #plt.ylabel('Number of Pokemon')
+    #plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/2')
 
-    plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/2')
+    fig1 = px.histogram(data_frame=df1,
+                        x='generation')
+    fig1.update_layout(xaxis_title='Generation',
+                       yaxis_title='Number of Pokemons',
+                       title='Pokemon by Generations')
+    fig.write_image(f'/Users/jason/dev/AirflowWeekend/data/2')
--- airflow-proj-files/dags/test_dag.py
+++ airflow-proj-files/dags/test_dag.py
@@ -1,12 +1,19 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from script import some_script
-
 
 default_args = {
     'owners': 'airflow',
     'start_date': datetime.today() - timedelta(days=1)
 }
 
-with DAG()
+with DAG('pokemon_dag', start_date=datetime(2022, 1, 1),
+         schedule_interval='@once',
+         catchup=False,
+         default_args=default_args) as dag:
+    task = PythonOperator(
+        task_id='pokemon script',
+        python_callable=some_script(),
+        dag=dag
+    )
