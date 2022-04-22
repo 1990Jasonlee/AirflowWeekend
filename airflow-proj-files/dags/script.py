7ac41a07263672cf4b31f22a044afcde042f328d
Jason <leejason1990@gmail.com>
2022-04-21 21:43:41 -0400

    another try

--- airflow-proj-files/dags/script.py
+++ airflow-proj-files/dags/script.py
@@ -1,5 +1,5 @@
 import pandas as pd
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 #import plotly.express as px
 
 def some_script():
@@ -7,11 +7,11 @@ def some_script():
     df = data[['name', 'type_1']]
     count = df.groupby('type_1')['type_1'].count()
 
-#    count.plot.bar()
-#    plt.title('Number of Pokemon by Primary Types')
-#    plt.xlabel('Primary Type')
-#    plt.ylabel('Number of Pokemon')
-#    plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/1')
+    count.plot.bar()
+    plt.title('Number of Pokemon by Primary Types')
+    plt.xlabel('Primary Type')
+    plt.ylabel('Number of Pokemon')
+    plt.savefig(f'./1')
 
 #    fig = px.bar(count, y='type_1')
 #    fig.update_layout(xaxis_title='Types',
@@ -20,12 +20,12 @@ def some_script():
 #    fig.write_image(f'/Users/jason/dev/AirflowWeekend/data/1')
 
     df1 = data[['name', 'generation']]
-#    bins = [1, 2, 3, 4, 5, 6, 7, 8]
-#    df1['generation'].hist(bins=bins)
-#    plt.title('Number of Pokemon by Generation')
-#    plt.xlabel('Generation')
-#    plt.ylabel('Number of Pokemon')
-#    plt.savefig(f'/Users/jason/dev/AirflowWeekend/data/2')
+    bins = [1, 2, 3, 4, 5, 6, 7, 8]
+    df1['generation'].hist(bins=bins)
+    plt.title('Number of Pokemon by Generation')
+    plt.xlabel('Generation')
+    plt.ylabel('Number of Pokemon')
+    plt.savefig(f'./2')
 
 #    fig1 = px.histogram(data_frame=df1,
 #                        x='generation')
@@ -34,5 +34,5 @@ def some_script():
 #                       title='Pokemon by Generations')
 #    fig.write_image(f'/Users/jason/dev/AirflowWeekend/data/2')
 
-    merge = pd.merge(df, df1, on = 'name')
-    merge.to_csv('./dags/new.csv')
+#    merge = pd.merge(df, df1, on = 'name')
+#    merge.to_csv('./dags/new.csv')
